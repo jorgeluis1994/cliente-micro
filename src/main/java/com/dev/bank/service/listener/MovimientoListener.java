58bd75475aa254659a1f14a12f6d5bc9e4de4812
jorge <jorgeluis199454@gmail.com>
2026-03-11 21:28:34 -0500

    fix(api): cosumo mensaje de movimiento de cuenta

--- src/main/java/com/dev/bank/service/listener/MovimientoListener.java
+++ src/main/java/com/dev/bank/service/listener/MovimientoListener.java
@@ -3,14 +3,19 @@ package com.dev.bank.service.listener;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import com.dev.bank.service.dto.MovimientoDTO;
+
 @Component
 public class MovimientoListener {
 
-    // @RabbitListener(queues = "movimientosQueue")
-    // public void procesarMovimiento(MovimientoDTO movimiento) {
-    //     System.out.println(">>> Cliente recibió movimiento: " + movimiento);
+    @RabbitListener(queues = "movimientosQueue")
+    public void procesarMovimiento(MovimientoDTO movimiento) {
+        System.out.println(">>> Cliente recibió movimiento: " 
+    + movimiento.getTipoMovimiento() 
+    + " $" + movimiento.getValor() 
+    + " | Saldo: " + movimiento.getSaldo());
         
-    // }
+    }
 
     
 }
